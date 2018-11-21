e8bacfb780d40b3ddbcc306d70c19cca47b943ce
Hamood3Amine <44638712+Hamood3Amine@users.noreply.github.com>
2018-11-21 12:17:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,26 +7,26 @@ client.on('ready', () => {
 
 
 
-const adminprefix = "-";
-const devs = ['506034854043975681'];
+const adminprefix = "s"; //Prefix
+const devs = ['506034854043975681']; // ID
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
     
-if (message.content.startsWith(adminprefix + 'setG')) {
+if (message.content.startsWith(adminprefix + 'G')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
-  if (message.content.startsWith(adminprefix + 'setN')) {
+  if (message.content.startsWith(adminprefix + 'N')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'setA')) {
+  if (message.content.startsWith(adminprefix + 'A')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
-if (message.content.startsWith(adminprefix + 'setT')) {
+if (message.content.startsWith(adminprefix + 'T')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
